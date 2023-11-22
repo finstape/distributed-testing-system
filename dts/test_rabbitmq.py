acaec2958b693f05a8d14184b06e2b65c3b53989
finstape <finstape@mail.ru>
2023-11-22 17:14:40 +0300

    delete files

--- dts/test_rabbitmq.py
+++
@@ -1,8 +0,0 @@
-import pika
-
-credentials = pika.PlainCredentials(username='dj-dts', password='zumfy6-zYkteg-mivvav')
-parameters = pika.ConnectionParameters(host='localhost', credentials=credentials, port='5672')
-connection = pika.BlockingConnection(parameters)
-
-print("соединение с rabbitmq установлено")
-connection.close()
